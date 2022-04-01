63ec9bf1f2956f3a97e0f11dbb12a7c1eb9f027e
Leonardo <regalado.zuniga.leonardo.daniel@gmail.com>
2022-04-01 17:49:41 -0600

    make it bett

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,11 @@
 const express=require('express');
+const app = express();
 const router=express.Router();
+
+const cors = require('cors');
+
+app.use(cors());
+
 router.get('/',(req,res)=> {
     res.render('index'); 
 });

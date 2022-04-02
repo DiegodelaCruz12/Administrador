b153068e498df1a46bf01de1d7e72133bb212296
Leonardo <regalado.zuniga.leonardo.daniel@gmail.com>
2022-04-01 18:08:51 -0600

    Bueno

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,5 @@
 const express=require('express');
-const app = express();
 const router=express.Router();
-
-const cors = require('cors');
-
-app.use(cors());
 
 router.get('/',(req,res)=> {
     res.render('index'); 
